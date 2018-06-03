cd725a587611525169ea2a09b3e271942dd5f3c1
caichaowei@gmail.com <caichaowei@gmail.com>
2018-06-03 21:49:56 +0800

    修改 参数工具单元测试

--- src/xmdp.addon/src/test/java/net/javafxchina/xmdp/addon/test/AddonServiceSetTest.java
+++ src/xmdp.addon/src/test/java/net/javafxchina/xmdp/addon/test/AddonServiceSetTest.java
@@ -3,16 +3,16 @@ package net.javafxchina.xmdp.addon.test;
 
 import org.junit.Test;
 
-import net.javafxchina.xmdp.core.Params;
 
 import net.javafxchina.xmdp.addon.AddonsLauncher;
+import net.javafxchina.xmdp.core.XMDPParams;
 public class AddonServiceSetTest {
 
 	@Test
 	public void test() throws Exception {
 		AddonsLauncher serviceSet = AddonsLauncher.getInstance();
 		serviceSet.init();
-		Params params=(Params) serviceSet.getService("params");
+		XMDPParams params=(XMDPParams) serviceSet.getService("xmdpParams");
 		System.out.println("debug="+params.isDebug());
 		System.out.println("xmdp.isDebug="+params.getParam("xmdp.isDebug"));
 		System.out.println("xmdp.testPWD.PWD="+params.getParam("xmdp.testPWD.PWD"));
